5c3229257e09b526e1922fc738436ecdc212164d
Anthony Carter <acarter5@academic.rrc.ca>
2025-09-14 22:14:14 -0500

    got the character count, no spaces function working

--- reading_text_programs.py
+++ reading_text_programs.py
@@ -28,3 +28,10 @@ def count_characters(words):
     return len(text)
 
 print(count_characters(text))
+
+def count_characters_no_spaces(words):
+    no_spaces = words.replace(" ", "")
+    return len(no_spaces)
+
+print(count_characters_no_spaces(text))
+
