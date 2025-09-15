6831225115c21812b898132ec57834ac7db8c19f
Anthony Carter <acarter5@academic.rrc.ca>
2025-09-14 21:43:43 -0500

    have a character count function with spaces

--- reading_text_programs.py
+++ reading_text_programs.py
@@ -14,8 +14,17 @@ def count_words(words):
 
 print(count_words(text))
 
+# Unique word count
+
 def count_unique_words(words):
     unique_word_count = words.split()
     return len(set(unique_word_count))
 
 print(count_unique_words(text))
+
+# Character count (with and without spaces)
+
+def count_characters(words):
+    return len(text)
+
+print(count_characters(text))
