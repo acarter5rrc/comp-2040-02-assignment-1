601a8d55511d07bd3854d6bd248b3309cdacd336
Anthony Carter <acarter5@academic.rrc.ca>
2025-09-14 19:52:59 -0500

    have a code that opens input.txt

---
+++ reading_text_programs.py
@@ -0,0 +1,8 @@
+# Word count
+# Unique word count
+# Character count (with and without spaces)
+# Average word length
+# Most common word(s) and their frequency
+
+with open("input.txt", "r") as f:
+    text = f.read()
